c74b66703508a4e2c37137fa231822a9b6e0ee02
idk <hankhill19580@gmail.com>
2021-03-12 17:45:42 -0500

    WOO! SOCKS PROXY WITH FULL UDP SUPPORT!

--- main.go
+++ main.go
@@ -1,31 +1,38 @@
 package main
 
 import (
-	"github.com/eyedeekay/goSam"
-	"github.com/getlantern/go-socks5"
+	"github.com/eyedeekay/sam3/helper"
+	"github.com/eyedeekay/sam3/i2pkeys"
+	"github.com/txthinking/socks5"
 	"log"
 )
 
 func main() {
 	// Create a SOCKS5 server
-	sam, err := goSam.NewDefaultClient()
+	addr := "127.0.0.1:8888"
+	ip := "127.0.0.1"
+	username := ""
+	password := ""
+	tcpTimeout := 60000
+	udpTimeout := 60000
+	i2pkeys.FakePort = true
+
+	primary, err := sam.I2PPrimarySession("sam-socks", "127.0.0.1:7656", "socks5")
 	if err != nil {
 		panic(err)
 	}
 
+	socks5.Dial = primary
+	socks5.Resolver = primary
+
+	server, err := socks5.NewClassicServer(addr, ip, username, password, tcpTimeout, udpTimeout)
+	if err != nil {
+		panic(err)
+	}
 	log.Println("Client Created")
 
-	// create a transport that uses SAM to dial TCP Connections
-	conf := &socks5.Config{
-		Dial:     sam.DialContext,
-		Resolver: sam,
-	}
-	server, err := socks5.New(conf)
-	if err != nil {
-		panic(err)
-	}
 	// Create SOCKS5 proxy on localhost port 8000
-	if err := server.ListenAndServe("tcp", "127.0.0.1:8000"); err != nil {
+	if err := server.ListenAndServe(nil); err != nil {
 		panic(err)
 	}
 }
--- sammy/README.md
+++
@@ -1,6 +0,0 @@
-sammy
-========================
-
-- [.](.)
-- [README.md](README.md)
-- [sammy.go](sammy.go)
