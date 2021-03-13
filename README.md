3a8927439411ca907f2f1e2dbdd0bf8e44b3dc90
idk <hankhill19580@gmail.com>
2021-03-13 17:39:45 -0500

    Make it take torsocks-like flags

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # samsocks
+
+This is a tool which will automatically sets up a SOCKS5 proxy to the
+I2P network, supporting both TCP and UDP communication, automatically
+using the SAM API. It will eventually be used like `torsocks` via the
+`LD_PRELOAD` hack, acting as a transparently isolating socksifier.
--- main.go
+++ main.go
@@ -1,23 +1,43 @@
 package main
 
 import (
+	"github.com/eyedeekay/portcheck"
 	"github.com/eyedeekay/sam3/helper"
 	"github.com/eyedeekay/sam3/i2pkeys"
+	"github.com/phayes/freeport"
 	"github.com/txthinking/socks5"
+
+	"flag"
 	"log"
+	"strconv"
 )
 
 func main() {
 	// Create a SOCKS5 server
-	addr := "127.0.0.1:8888"
-	ip := "127.0.0.1"
-	username := ""
-	password := ""
-	tcpTimeout := 60000
-	udpTimeout := 60000
+	addr := flag.String("socksaddr", "127.0.0.1", "Start the SOCKS5 proxy at this address(Can use a domain)")
+	port := flag.String("socksport", 7950, "Start the SOCKS5 proxy at this port.")
+	ip := flag.String("ipaddr", "127.0.0.1", "Listen on this IP address")
+	username := flag.String("user", "", "Require a username to use the SOCKS5 Proxy.")
+	password := flag.String("pass", "", "Require a password to use the SOCKS5 Proxy.")
+	isolate := flag.Bool("isolate", true, "Enforce isolation.")
+	tcpTimeout := flag.Int("tcptimeout", 60000, "Set a default TCP Timeout(ms)")
+	udpTimeout := flag.Int("udptimeout", 60000, "Set a default UDP Timeout(ms)")
+	samaddress := flag.String("address", "127.0.0.1", "Specify I2P SAM address")
+	samport := flag.Int("port", "7656", "Specify I2P SAM port")
+	//	shell := flag.Bool("shell", false, "spawn an I2P-only shell")
+
+	var err error
+	if *isolate {
+		if portcheck.SCR(addr, port) {
+			port, err = freeport.GetFreePort()
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
 	i2pkeys.FakePort = true
 
-	primary, err := sam.I2PPrimarySession("sam-socks", "127.0.0.1:7656", "socks5")
+	primary, err := sam.I2PPrimarySession("sam-socks", *samaddress+":"+strconv.Itoa(*samport), "socks5")
 	if err != nil {
 		panic(err)
 	}
@@ -25,14 +45,20 @@ func main() {
 	socks5.Dial = primary
 	socks5.Resolver = primary
 
-	server, err := socks5.NewClassicServer(addr, ip, username, password, tcpTimeout, udpTimeout)
+	server, err := socks5.NewClassicServer(*addr+":"+*port, *ip, *username, *password, *tcpTimeout, *udpTimeout)
 	if err != nil {
 		panic(err)
 	}
-	log.Println("Client Created")
+	log.Println("Client Created SOCKS5 proxy at", *addr)
+	// Create SOCKS5 proxy
+	go func() {
+		if err := server.ListenAndServe(nil); err != nil {
+			panic(err)
+		}
+	}()
 
-	// Create SOCKS5 proxy on localhost port 8000
-	if err := server.ListenAndServe(nil); err != nil {
-		panic(err)
+	for {
+
 	}
+
 }
